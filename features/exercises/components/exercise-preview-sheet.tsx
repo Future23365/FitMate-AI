0040ac48e0439e6e54f9d0b563894fad9c19b326
liusongbai <liusongbai2072@qq.com>
2026-05-23 14:18:21 +0800

    去除抽屉动作图内凹背景

--- features/exercises/components/exercise-preview-sheet.tsx
+++ features/exercises/components/exercise-preview-sheet.tsx
@@ -173,7 +173,7 @@ export function ExercisePreviewSheet({
                 </h4>
 
                 {/* 大图展示区域 */}
-                <div className="group relative aspect-square w-full overflow-hidden rounded-xl border border-slate-100 bg-white shadow-inner">
+                <div className="group relative aspect-square w-full overflow-hidden rounded-xl bg-white">
                   <Image
                     alt={`${exercise.nameZh} 演示图`}
                     className="object-contain"
