708d299b04533dca34dc13cc8c765d1dc17882f9
liusongbai <liusongbai2072@qq.com>
2026-06-06 18:56:35 +0800

    调整配置

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     formats: ["image/avif", "image/webp"],
     imageSizes: [32, 48, 64, 96, 128, 256, 384],
